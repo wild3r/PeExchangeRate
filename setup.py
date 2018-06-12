518d4d8c55312cdcb6e3e946e304f168152884b0
Wilder <wilderhernandezg@gmail.com>
2018-06-12 00:28:46 -0500

    [update] files for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'PeExchangeRate',
+  packages = ['PeExchangeRate'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Peru sunet exchange rate',
+  author = 'Wilder Hernández García',
+  author_email = 'wilderhernandezg@gmail.com',
+  url = 'https://github.com/wilderhernandezgarcia/PeExchangeRate', # use the URL to the github repo
+  download_url = 'https://github.com/wilderhernandezgarcia/PeExchangeRate/tarball/0.1',
+  keywords = ['Sunat', u'Perú',],
+  classifiers = [],
+)
